7c04e326eaba2db0bf40754a2a9f4d07a3b4738f
manu0470 <102345573+manu0470@users.noreply.github.com>
2022-11-14 22:56:32 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>APIS ABSTRACT MANUEL</title>
+	<link rel="stylesheet" href="flatly.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=<?=$apiManu?>" ></script>
+	<link rel="icon" style="image/x-icon" href="img/logo.png">
+	<link rel="stylesheet" href="css/estilos.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a85adf3762.js" crossorigin="anonymous"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyC_UwaejdWAO71-91WxZN6xkuxWkDo8GyM"></script>
+	
+</head>
+<body>
+	<nav class="navbar bg-light fixed-top navbar-center">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">VALIDACIÓN DE APIS DE ABSTRACT</span>
+  </div>
+</nav>
+<br><br><br><br>
+
+<div class="container">
+	<div class="row">
+			<div class="col-md-12" align="center">
+				<i class="fa-solid fa-phone fa-fade" style="font-size:300px;"></i><br><br><br>
+				<button class="btn btn-primary" style="background: #04b49a; border-color: #04b49a;" onclick="location.href='registro.php'">INGRESAR</button>
+			</div>
+		</div>
+</div>
+
+
+<br><br><br><br>
+
+
+<h6 align="center">ELABORADO POR: CARLOS MANUEL ZAVALA SALAS 3521110001 4° A</h6>
+
+		
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
